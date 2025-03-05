dacda62fdbc2d6221fca9abea876d5bd0fad742d
jirishus <jirishus@yahoo.com>
2025-03-04 19:54:22 -0800

    tailwind postcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind will look through these files
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'selector',
+  plugins: [],
+};
